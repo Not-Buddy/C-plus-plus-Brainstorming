189edd4f8b6e12f5c215e82de37e371a35a09844
Not-Buddy <kingeaary@gmail.com>
2024-09-16 11:26:04 +0000

    Another one

--- new78StayamandCounting.cpp
+++ new78StayamandCounting.cpp
@@ -1,47 +1,56 @@
-
-#include <iostream>
-#include <string>
-#include <cctype>
-#include <vector>
-#include <algorithm>
-//typedef long long ll;
+#include <bits/stdc++.h>
 using namespace std;
-
-
-void vectormaker(std::vector <int>&vec1,std::string input)
-{
-  std::string digit{};
-  for(int i{0};i<input.size();i++)
-  {
-    while(std::isdigit(input[i]))
-    {
-      digit.push_back(input[i]);
-      i++;
+typedef long long ll;
+ 
+ 
+int solve(){
+    int n,x,y;
+    cin>>n;
+    unordered_map<int,int> m0;
+    unordered_map<int,int> m1;
+    for(int i=0;i<n;i++){
+        cin>>x>>y;
+        if(y==0){
+            m0[x]++;
+        }
+        else{
+            m1[x]++;
+        }
     }
-    vec1.push_back(std::stoi(digit));
-
-    digit.clear();
-  }
+    long long ans=0;
+    for(auto temp:m0){
+        if (m1.count(temp.first) > 0)
+        {
+            ans+=(long long)n-2;
+        }
+    }
+    for(auto temp:m0){
+        int x1=temp.first-1;
+        int x2=temp.first+1;
+        if(m1.find(x1)!=m1.end() && m1.find(x2)!=m1.end()){
+            ans++;
+        }
+    }
+ 
+    for (auto temp : m1)
+    {
+        int x1 = temp.first - 1;
+        int x2 = temp.first + 1;
+        if (m0.find(x1) != m0.end() && m0.find(x2) != m0.end())
+        {
+            ans++;
+        }
+    }
+ 
+    cout<<ans<<endl;
+    return 0;
 }
-void VectorDisplay(std::vector <int> const vec1)
-{
-  cout<<"{ ";
-  for(int i:vec1)
-  {
-    cout<<i<<" ";
-  }
-  cout<<"}"<<endl;
-}
-
 int main()
 {
-  int testcases{};
-  cin>>testcases;
-  cin.ignore();
-  for(int i{0};i<testcases;i++)
-  {
-
-  }
-
-  return 0;
+    int testCase=1;
+    cin>>testCase;
+    while(testCase--){
+        solve();
+    }
+    return 0;
 }
