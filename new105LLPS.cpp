b005086c3b52668ff840c50880591f10fbb709fc
Not-Buddy <kingeaary@gmail.com>
2024-12-02 23:18:58 +0530

    Another one

---
+++ new105LLPS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+typedef unsigned long long ll;
+using namespace std;
+
+
+int main()
+{
+  std::string Line{};
+  getline(cin,Line);
+
+  return 0;
+}
