bd63f550f2a142086739ad1e1e40e51668f36693
Not-Buddy <kingeaary@gmail.com>
2025-08-13 21:00:37 +0530

    TLE on C

--- new112Round1042Div3C.cpp
+++ new112Round1042Div3C.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cctype>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 typedef unsigned long long ll;
 using namespace std;
@@ -11,44 +11,64 @@ int main()
 {
   int testcases{};
   cin>>testcases;
-  for(int i{0};i<testcases;i++)
+  for(int j{0};j<testcases;j++)
   {
 	int n{},k{};
 	cin>>n>>k;
 
-	std::vector<int> S(n),T{};
-	map<int, int> mpt{};
+	std::vector<int> S(n),T(n);
+	unordered_map<int, int> freqA{},freqB{};
 
-	for(int j{0};j<n;j++) cin>>S[j];
+	for(int i{0};i<n;i++) cin>>S[i];
+	for(int i{0};i<n;i++) cin>>T[i];
 
-
-	for(int j{0};j<n;j++)
-	{
-		int x;
-		cin>>x;
-		x%=k;
-		mpt[x]++;
+	for(int i{0};i<n;i++)
+	{	
+			freqA[S[i]%k]++;
+			freqB[T[i]%k]++;
 	}
-
-	for(int j{0};j<n;j++)
+	for(auto x:freqA)
 	{
-		int x=S[j]%k;
-		if(mpt[x])
+		if(freqB.find(x.first)!=freqB.end())
 		{
-			mpt[x]--;
-			continue;
-		}
-		if(mpt[k-x])
-			mpt[k-x]--;
-	}
+			freqA[x.first]-=freqB[x.first]; //zero A out if found
+			freqB[x.first]-=0;		//zero B out if found
+			if(freqA[x.first]<0)
+			{
+				freqB[x.first]-=freqA[x.first];
+				freqA[x.first]=0;
+			}
 
-	int cnt{0};
-	for(auto e:mpt)
-	{
-		if(e.ss!=0)cnt++;
+		}
+		if(freqB.find(k-x.first)!=freqB.end())
+		{
+			freqA[x.first]-=freqB[k-x.first];
+			freqB[k-x.first]=0;
+			if(freqA[x.first]<0)
+			{
+				freqB[k-x.first]-=freqA[x.first];
+				freqA[x.first]=0;
+			}
+		}
 	}
-	if(cnt) cout<<"NO";
-	else cout<<"YES";
+	string ans="YES";
+	for(auto it:freqA)
+	{
+		if(it.second>0)
+		{
+			ans="NO";
+			break;
+		}
+	}
+	for(auto it:freqB)
+	{
+		if(it.second>0)
+		{
+			ans="NO";
+			break;
+		}
+	}
+	cout<<ans<<"\n";
 
   }
 
