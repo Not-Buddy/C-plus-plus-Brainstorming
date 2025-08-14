2d5f15e2d9efdeeac74a4cbb65e2bfcabd92875f
Not-Buddy <kingeaary@gmail.com>
2025-08-14 21:32:37 +0530

    C problem

--- new114round1034iv3C.cpp
+++ new114round1034iv3C.cpp
@@ -15,10 +15,18 @@ int main()
   {
 	int n{};
 	cin>>n;
-	std::vector<int> arr(n);
+	std::vector<int> arr(n),mn(n),mx(n);
 	for(int i{0};i<n;i++) cin>>arr[i];
+	mn[0]=arr[0];
+	for(int i{1};i<n;i++)
+		mn[i]=min(mn[i-1],arr[i]);
+	mx[n-1]=arr[n-1];
+	for(int i{n-2};i>=0;i--)
+		mx[i]=max(mx[i+1],arr[i]);
 
-
+	for(int i{0};i<n;i++)
+		cout<<(arr[i]==mn[i]||arr[i]==mx[i])?'1':'0';
+	cout<<endl;
   }
 
   return 0;
