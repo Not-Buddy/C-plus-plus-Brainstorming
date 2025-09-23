8712ff5136686992daacec60c2b2c5f739c91cea
Buddy <kingeaary@gmail.com>
2025-09-23 23:16:02 +0530

    LC revisted a few questions

--- RevisingLeet/twosum.cpp
+++ RevisingLeet/twosum.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+            int n = nums.size();
+            for(int i{};i<n;i++)
+            {
+                for(int j{};j<n;j++)
+                {
+                    if(i!=j&&(nums[i]+nums[j]==target))
+                        return {i,j};
+                }
+            }
+            return {};
     }
 };
