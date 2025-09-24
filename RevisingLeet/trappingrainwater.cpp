fcaa10506bd3cd2c64d9bb64b096ed56dddec1fe
Not-Buddy <kingeaary@gmail.com>
2025-09-24 19:09:37 +0530

    brain fried from rain water

---
+++ RevisingLeet/trappingrainwater.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size()-1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
+        
+    }
+};
