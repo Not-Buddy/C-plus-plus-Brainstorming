e9fabc9dea1c1d3877a05f1e03f01292aa4000ad
NBonARM <kingeaary@gmail.com>
2025-10-19 16:00:40 +0000

    MaxSubArray Using divide and conquer

---
+++ RevisingLeet/MaxSubArrayDivideAndConquer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums,0,nums.size()-1);
+    }
+
+    int maxSubArray(vector<int> &A, int L, int R)
+    {
+        if(L>R) return INT_MIN;
+        int mid = (L+R)/2,leftSum=0,rightSum=0;
+        for(int i = mid - 1, curSum = 0 ;i>=L;i--)
+            curSum +=A[i], leftSum = max(leftSum,curSum);
+        for(int i=mid+1,curSum=0;i<=R;i++)
+            curSum+=A[i], rightSum = max(curSum,rightSum);
+        return max({maxSubArray(A,L,mid-1),maxSubArray(A,mid+1,R),leftSum+rightSum+A[mid]});
+    }
+};
