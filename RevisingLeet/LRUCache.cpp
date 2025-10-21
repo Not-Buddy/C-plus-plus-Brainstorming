77f31264bed3f34ab9b8c2bdee8509d68b67e14f
NBonARM <kingeaary@gmail.com>
2025-10-21 06:24:34 +0000

    LRU Cache but with O(n) Eviction strategy

--- RevisingLeet/LRUCache.cpp
+++ RevisingLeet/LRUCache.cpp
@@ -1,15 +1,40 @@
 class LRUCache {
+private:
+    unordered_map<int, pair<int,int>> cache;
+    int capacity;
+    int timestamp;
 public:
-    LRUCache(int capacity) {
-        
-    }
+    LRUCache(int capacity) : capacity(capacity), timestamp(0) {}
     
     int get(int key) {
-        
+        if(cache.find(key) == cache.end())
+            return -1;
+
+        cache[key].second = timestamp++;
+        return cache[key].first;
     }
     
     void put(int key, int value) {
-        
+        if(cache.find(key) != cache.end())
+            cache[key] = {value,timestamp++};
+        else 
+        {
+            if(cache.size()>=capacity)
+            {
+                int minTime = INT_MAX;
+                int keyToRemove = -1;
+                for(const auto& pair:cache)
+                {
+                    if(pair.second.second < minTime)
+                    {
+                            minTime = pair.second.second;
+                            keyToRemove = pair.first;
+                    }
+                }
+                cache.erase(keyToRemove);
+            }
+            cache[key] = {value, timestamp++};
+        }
     }
 };
 
