3636516c0b51b2e6fb6e276e2947646e0b76e0e3
NBonARM <kingeaary@gmail.com>
2025-10-22 05:43:06 +0000

    Merge intervals efficient code lc

---
+++ RevisingLeet/MergeIntervalsEfficient.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    vector<int> Merger(const vector<int> v1,const vector<int> v2)
+    {
+        return {min(v1[0], v2[0]), max(v1[1], v2[1])};
+    }
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> merged;
+        for(const auto& interval : intervals)
+        {
+            if(merged.empty()||merged.back()[1] < interval[0])
+                merged.push_back(interval);
+            else
+                merged.back()[1] = max(merged.back()[1],interval[1]);
+        }
+
+        return merged;
+    }
+};
