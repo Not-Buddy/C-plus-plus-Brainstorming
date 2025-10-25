8db3225b716aca652d60dee331d59dd5d129a0b5
NBonARM <kingeaary@gmail.com>
2025-10-25 12:06:45 +0000

    Lmao same code fworks for SubsetsI as for Subsets II LC Medium

---
+++ RevisingLeet/SubsetsI.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        std::set<std::vector<int>> uniq_subsets;
+        std::vector<int> current;
+        std::sort(nums.begin(),nums.end());
+        backtrack(nums,0,current,uniq_subsets);
+        return std::vector<std::vector<int>> (uniq_subsets.begin(),uniq_subsets.end());                
+   }
+   void backtrack(const std::vector<int> &nums,int start, std::vector<int> &current,
+   std::set<std::vector<int>> &uniq_subsets)
+   {
+    uniq_subsets.insert(current);
+    for(int i = start; i<nums.size();i++)
+    {
+        current.push_back(nums[i]);
+        backtrack(nums,i+1,current,uniq_subsets);
+        current.pop_back();
+    }
+   }
+};
